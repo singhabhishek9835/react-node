f5bacc3901d8409edd749fe95a5055a4e6e7b4f8
Telmo Sampaio <telmoasampaio@gmail.com>
2021-06-06 21:39:46 +0100

    Adding workflow with pm2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('/names', (req, res) => {
-  res.send("Telmo");
+  res.send("Telmo Sampaio");
 })
 
 app.listen(5000, () => {
